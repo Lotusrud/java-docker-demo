1d0f68931f3e2d0e0840c4a653e97de4a6b409c5
Lotusrud <ramya.mycloud@gmail.com>
2026-01-29 06:20:09 +0000

    keep containerrunning for demo

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello from Docker & Jenkins!");
+
+	//keep container running
+	while (true) {
+		Thread,sleep(10000);
+	}
     }
 }
 
